9d2b2647ed2dc97b36528ad2caa266259891ca16
thiagoprocaci <thiagoprocaci@fab19787-7408-b86d-5ea9-555c336d1e50>
2010-03-27 21:14:02 +0000

    Ajustes no módulo de persistencia do framework

--- shadya/src/main/java/com/framework/persistence/dao/dql/support/QueryHelper.java
+++ shadya/src/main/java/com/framework/persistence/dao/dql/support/QueryHelper.java
@@ -1,7 +1,9 @@
 package com.framework.persistence.dao.dql.support;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.jdo.Query;
 
@@ -9,12 +11,10 @@ import com.framework.persistence.dao.dql.OrderBy;
 
 /**
  * 
- * Classe helper para facilitar a construcao de queries
+ * Classe helper para facilitar a manipulacao de queries
  * 
  */
 public final class QueryHelper {
-	
-
 	/**
 	 * Executa query e retorna resultado em uma lista
 	 * 
@@ -23,13 +23,13 @@ public final class QueryHelper {
 	 * @return Retorna resultado da query
 	 */
 	@SuppressWarnings("unchecked")
-	public static List<Object> getQueryResult(Query query, Object... parameters) {
+	public static List<Object> getQueryResult(Query query, Map<String, Object> parameters) {
 		List<Object> list = new ArrayList<Object>();
 		List<Object> results = null;
-		if (parameters == null || parameters.length <= 0) {
+		if (parameters == null || parameters.isEmpty()) {
 			results = (List<Object>) query.execute();
 		} else {
-			results = (List<Object>) query.executeWithArray(parameters);
+			results = (List<Object>) query.executeWithMap(parameters);
 		}
 		if (results != null) {
 			for (Object object : results) {
@@ -37,6 +37,17 @@ public final class QueryHelper {
 			}
 		}
 		return list;
+	}
+
+	/**
+	 * Executa query e retorna resultado em uma lista
+	 * 
+	 * @param query
+	 * @param parameters
+	 * @return Retorna resultado da query
+	 */
+	public static List<Object> getQueryResult(Query query) {
+		return getQueryResult(query, null);
 	}
 
 	/**
@@ -56,5 +67,40 @@ public final class QueryHelper {
 			}
 		}
 		return clause == null ? null : clause.toString();
+	}
+
+	/**
+	 * 
+	 * @param parameterQueryList
+	 * @return Retorna parametros como string
+	 */
+	public static String getParametersAsString(ParameterQuery... parameterQueryList) {
+		StringBuilder parameters = null;
+		if (parameterQueryList != null && parameterQueryList.length > 0) {
+			parameters = new StringBuilder();
+			for (int i = 0; i < parameterQueryList.length; i++) {
+				parameters.append(parameterQueryList[i].getType().getName() + " " + parameterQueryList[i].getParameterName());
+				if (i != parameterQueryList.length - 1) {
+					parameters.append(", ");
+				}
+			}
+		}
+		return parameters == null ? null : parameters.toString();
+	}
+
+	/**
+	 * 
+	 * @param parameterQueryList
+	 * @return Retorna parametros como um map
+	 */
+	public static Map<String, Object> getParametersAsMap(ParameterQuery... parameterQueryList) {
+		Map<String, Object> parameters = null;
+		if (parameterQueryList != null && parameterQueryList.length > 0) {
+			parameters = new HashMap<String, Object>();
+			for (int i = 0; i < parameterQueryList.length; i++) {
+				parameters.put(parameterQueryList[i].getParameterName(), parameterQueryList[i].getValue());
+			}
+		}
+		return parameters;
 	}
 }
